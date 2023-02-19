5f8dbd0ad57aa0fd0ad197f545919368ea597924
Igor <igornascimento02@gmail.com>
2023-02-19 03:49:04 -0300

    add pexe

--- pexe/main.js
+++ pexe/main.js
@@ -1,8 +1,10 @@
 var px;
 var dotts = 0;
+var isMobile
 window.onload = function () {
     px = document.getElementById('px');
     dots({ ctrlKey: false });
+    isMobile = detectMob();
 }
 
 window.onmousemove = function (e) {
@@ -15,21 +17,21 @@ window.onmousemove = function (e) {
 document.onmousedown = dots;
 
 function dots(e) {
-    if (!e.ctrlKey) {
-        for (let i = 0; i < 100; i++) {
+    if (!e.ctrlKey && !isMobile) {
+        for (let i = 0; i < 200; i++) {
             var main = document.getElementById('main')
             const dot = document.createElement('div');
             dot.classList.add('dot')
-            dot.style.left = Math.floor(Math.random() * document.documentElement.scrollWidth) + 'px';
-            dot.style.top = Math.floor(Math.random() * document.documentElement.scrollHeight) + 'px';
+            dot.style.left = Math.floor(Math.random() * document.documentElement.scrollWidth - 25) + 'px';
+            dot.style.top = Math.floor(Math.random() * document.documentElement.scrollHeight - 25) + 'px';
             dot.style.animationDuration = Math.floor(Math.random() * 15) + 5 + 's';
             main.appendChild(dot);
             dotts++;
         }
     }
 
-    if (e.ctrlKey && dotts > 0) {
-        for (let i = 0; i < 100; i++) {
+    if (e.ctrlKey && dotts > 0 && !isMobile) {
+        for (let i = 0; i < 200; i++) {
             const dot = document.getElementsByClassName('dot')[0];
             dot.remove();
             dotts--;
@@ -37,4 +39,20 @@ function dots(e) {
     }
 
     console.log(dotts + ' dots');
+}
+
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
 }
