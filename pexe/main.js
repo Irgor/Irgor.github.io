5ddd43c541c016173b8e46e192577a4750655646
Igor <igornascimento02@gmail.com>
2023-02-20 00:16:35 -0300

    change pexe

--- pexe/main.js
+++ pexe/main.js
@@ -3,7 +3,7 @@ var dotts = 0;
 var isMobile
 window.onload = function () {
     px = document.getElementById('px');
-    dots({ ctrlKey: false });
+    dots();
     isMobile = detectMob();
 }
 
@@ -14,28 +14,36 @@ window.onmousemove = function (e) {
     }
 }
 
-document.onmousedown = dots;
+window.onmousedown = function () {
+    document.getElementById('px').style.boxShadow = '0px 0px 500px 250px #ffff001f'
+}
 
-function dots(e) {
-    if (!e.ctrlKey && !isMobile) {
-        for (let i = 0; i < 200; i++) {
-            var main = document.getElementById('main')
-            const dot = document.createElement('div');
-            dot.classList.add('dot')
-            dot.style.left = Math.floor(Math.random() * document.documentElement.scrollWidth - 25) + 'px';
-            dot.style.top = Math.floor(Math.random() * document.documentElement.scrollHeight - 25) + 'px';
-            dot.style.animationDuration = Math.floor(Math.random() * 15) + 5 + 's';
-            main.appendChild(dot);
-            dotts++;
-        }
-    }
+window.onmouseup = function () {
+    document.getElementById('px').style.boxShadow = '0px 0px 200px 100px #ffff001f'
+}
 
-    if (e.ctrlKey && dotts > 0 && !isMobile) {
-        for (let i = 0; i < 200; i++) {
-            const dot = document.getElementsByClassName('dot')[0];
-            dot.remove();
-            dotts--;
-        }
+function dots() {
+    for (let i = 0; i < 350; i++) {
+        var main = document.getElementById('main')
+        const dot = document.createElement('div');
+        dot.classList.add('dot')
+
+        dot.style.left = Math.floor(Math.random() * document.documentElement.scrollWidth - 25) + 'px';
+        dot.style.top = Math.floor(Math.random() * document.documentElement.scrollHeight - 25) + 'px';
+
+        const height = Math.floor(Math.random() * 16) + 1;
+        dot.style.height = height + 'px'
+        dot.style.width = dot.style.height
+
+        const alpha = Math.floor(Math.random() * 15) + 1;
+        dot.style.backgroundColor = `rgba(255, 255, 255, 0.${alpha})`
+
+        dot.style.animationDuration = Math.floor(Math.random() * 15) + 8 + 's';
+        dot.style.animationName = 'move' + (Math.floor(Math.random() * 4) + 1);
+
+
+        main.appendChild(dot);
+        dotts++;
     }
 
     console.log(dotts + ' dots');
@@ -51,7 +59,7 @@ function detectMob() {
         /BlackBerry/i,
         /Windows Phone/i
     ];
-    
+
     return toMatch.some((toMatchItem) => {
         return navigator.userAgent.match(toMatchItem);
     });
